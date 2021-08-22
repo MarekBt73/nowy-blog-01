17e792f921954ad54fd3e53cd7b7a4a4ab146a9d
Marek Becht <marek.becht@gmail.com>
2021-08-22 11:25:41 +0200

    dnia-22-08-21-1

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
      `gatsby-plugin-styled-components`
+     `gatsby-plugin-netlify-cms`
 
 
   ],
